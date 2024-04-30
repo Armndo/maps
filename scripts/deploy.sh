2a474cf982f06436a86b41a6e0cbe5f06988469c
Armando Gonzalez <armndo.g@gmail.com>
2024-04-30 12:29:28 -0600

    changed deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 export $(grep -v '^#' .env)
 
-timestamp=$(date +%Y_%m_%d_%H%M%S)
+timestamp=$(date +%Y-%m-%d-%H%M%S)
 
-rm build*.zip
-zip -r -q - build > build_$timestamp.zip
-ssh -i $SSH_IDENTITY $SSH_USER@$SSH_HOST "cd $SSH_DIR; rm -R ./build; rm ./*.zip; echo \"removed old build\""
-scp -i $SSH_IDENTITY build_$timestamp.zip $SSH_USER@$SSH_HOST:$SSH_DIR
-ssh -i $SSH_IDENTITY $SSH_USER@$SSH_HOST "cd $SSH_DIR; unzip -q ./build_$timestamp.zip -d ./; echo \"done building!\""
+rm ./$SSH_BUILD*.zip
+zip -r -q - $SSH_BUILD > $SSH_BUILD-$timestamp.zip
+ssh -i $SSH_IDENTITY $SSH_USER@$SSH_HOST "cd $SSH_DIR; rm -R ./*; echo \"removed old build\""
+scp -i $SSH_IDENTITY $SSH_BUILD-$timestamp.zip $SSH_USER@$SSH_HOST:$SSH_DIR
+ssh -i $SSH_IDENTITY $SSH_USER@$SSH_HOST "cd $SSH_DIR; unzip -q ./$SSH_BUILD-$timestamp.zip -d ./; mv ./$SSH_BUILD/* ./; echo \"done building!\""
